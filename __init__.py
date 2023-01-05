bf889e80e7fcbdbb2fd501de3610420491412b86
Aditya Mehra <aix.m@outlook.com>
2023-01-05 20:16:40 +1030

    add folder playlist (#2)
    * add folder playlist
    
    * clean up debug

--- __init__.py
+++ __init__.py
@@ -19,10 +19,12 @@ class FileBrowserSkill(MycroftSkill):
         """
         super(FileBrowserSkill, self).__init__(name="FileBrowserSkill")
         self.skill_location_path = None
+        self.udev_thread = None
 
     def initialize(self):
         self.add_event('skill.file-browser.openvoiceos.home', self.show_home)
         self.gui.register_handler('skill.file-browser.openvoiceos.handle.file', self.handle_file)
+        self.gui.register_handler('skill.file-browser.openvoiceos.handle.folder.playlists', self.handle_folder_playlist)
         self.gui.register_handler('skill.file-browser.openvoiceos.send.file.kdeconnect', self.share_to_device_kdeconnect)
         self.audioExtensions = ["aac", "ac3", "aiff", "amr", "ape", "au", "flac", "alac" , "m4a", "m4b", "m4p", "mid", "mp2", "mp3", "mpc", "oga", "ogg", "opus", "ra", "wav", "wma"]
         self.videoExtensions = ["3g2", "3gp", "3gpp", "asf", "avi", "flv", "m2ts", "mkv", "mov", "mp4", "mpeg", "mpg", "mts", "ogm", "ogv", "qt", "rm", "vob", "webm", "wmv"]
@@ -37,18 +39,18 @@ class FileBrowserSkill(MycroftSkill):
             monitor.filter_by(subsystem='usb')
             self.udev_thread = pyudev.MonitorObserver(monitor, self.handle_udev_event)
             self.udev_thread.start()
-
+    
         except Exception as e:
             pass
-
+            
     def handle_udev_event(self, action, device):
         """
         Handle a udev event
         """
         if action == 'add':
-            if device.device_node is not None:
+            if device.device_node is not None: 
                 self.gui.show_notification("New USB device detected - Open file browser to explore it", action="skill.file-browser.openvoiceos.home", noticetype="transient", style="info")
-
+            
         elif action == 'remove':
             if device.device_node is not None:
                 self.gui.show_notification("A USB device was removed", noticetype="transient", style="info")
@@ -65,7 +67,6 @@ class FileBrowserSkill(MycroftSkill):
         Handle a file from the file browser Video / Audio
         """
         fileUrl = message.data.get("fileURL", "")
-        # Determine if file is audio or video
         fileExtension = fileUrl.split(".")[-1]
         if fileExtension in self.audioExtensions:            
             media = {
@@ -100,6 +101,51 @@ class FileBrowserSkill(MycroftSkill):
             }
             playlist = [media]
             disambiguation = [media]
+            self.bus.emit(Message("ovos.common_play.play", {"media": media, "playlist": playlist, "disambiguation": disambiguation}))
+            self.gui.release()
+    
+    def handle_folder_playlist(self, message):
+        """
+        Handle a folder from the file browser as a playlist
+        """
+        folderUrl = message.data.get("path", "")
+        files = os.listdir(folderUrl)
+        playlist = []
+        for file in files:
+            fileUrl = "file://" + folderUrl + "/" + file
+            fileExtension = fileUrl.split(".")[-1]
+            if fileExtension in self.audioExtensions:
+                media = {
+                    "match_confidence": 100,
+                    "media_type": MediaType.AUDIO,
+                    "length": 0,
+                    "uri": fileUrl,
+                    "playback": PlaybackType.AUDIO,
+                    "image": self.skill_location_path + "/ui/images/generic-audio-bg.jpg",
+                    "bg_image": self.skill_location_path + "/ui/images/generic-audio-bg.jpg",
+                    "skill_icon": "",
+                    "title": fileUrl.split("/")[-1],
+                    "skill_id": "skill-file-browser.openvoiceos"
+                }
+                playlist.append(media)
+            if fileExtension in self.videoExtensions:
+                media = {
+                    "match_confidence": 100,
+                    "media_type": MediaType.VIDEO,
+                    "length": 0,
+                    "uri": fileUrl,
+                    "playback": PlaybackType.VIDEO,
+                    "image": self.skill_location_path + "/ui/images/generic-audio-bg.jpg",
+                    "bg_image": self.skill_location_path + "/ui/images/generic-audio-bg.jpg",
+                    "skill_icon": "",
+                    "title": fileUrl.split("/")[-1],
+                    "skill_id": "skill-file-browser.openvoiceos"
+                }
+                playlist.append(media)
+
+        if len(playlist) > 0:
+            media = playlist[0]
+            disambiguation = playlist
             self.bus.emit(Message("ovos.common_play.play", {"media": media, "playlist": playlist, "disambiguation": disambiguation}))
             self.gui.release()
             
